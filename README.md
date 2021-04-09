ff0157897499e3274704afca3d2bb01b533be284
Parveen Sangroya <9700282+sangroya@users.noreply.github.com>
2021-04-09 13:26:05 +0530

    initial commit
    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# yii2-json-schema-validator
+# Yii 2 JSON Schema Validator
+
+A Yii 2 extension that provides a validation class that wraps
+[JSON Schema for PHP](https://github.com/justinrainbow/json-schema).
+
+## Installation
+
+```
+$ composer require sangroya/yii2-json-schema-validator
+```
+
+## Usage
+
+Model class example:
+
+```php
+<?php
+
+namespace app\models;
+
+use sangroya\jsonschema\JsonSchemaValidator;
+use Yii;
+use yii\base\Model;
+
+class MyModel extends Model
+{
+    public $json;
+
+    public function rules()
+    {
+        return [
+            [
+                'json',
+                JsonSchemaValidator::className(),
+                'schema' => 'file://' . Yii::getAlias('@app/path/to/schema.json'),
+                /* or URL
+                'schema' => 'https://test-example.com/path/to/schema.json',
+                */
+            ],
+        ];
+    }
+}
+```
+
+See [json-schema](http://json-schema.org) for details how to describe JSON schema.
---
+++ src/JsonSchemaValidator.php
@@ -0,0 +1,147 @@
+<?php
+namespace sangroya\jsonschema\validator;
+
+use JsonSchema\Uri\UriRetriever;
+use JsonSchema\Validator as JSValidator;
+use Yii;
+use yii\base\InvalidConfigException;
+use yii\validators\Validator;
+
+/**
+ * JsonSchemaValidator validates a value against a JSON Schema file.
+ *
+ * The URI of the schema file must be defined via the [[schema]] property.
+ *
+ * @author Parveen
+ */
+class JsonSchemaValidator extends Validator
+{
+    /**
+     * @var string The URI of the JSON schema file.
+     */
+    public $schema;
+
+    /**
+     * @var string User-defined error message used when the schema is missing.
+     */
+    public $schemaEmpty;
+
+    /**
+     * @var string User-defined error message used when the schema isn't a string.
+     */
+    public $schemaNotString;
+
+    /**
+     * @var string User-defined error message used when the value is not a string.
+     */
+    public $notString;
+
+    /**
+     * @var string User-defined error message used when the value is not a valid JSON string.
+     */
+    public $notJsonString;
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        if ($this->schemaEmpty === null) {
+            $this->schemaEmpty = 'The "schema" property must be set.';
+        }
+
+        if ($this->schemaNotString === null) {
+            $this->schemaNotString = 'The "schema" property must be a a string.';
+        }
+
+        if ($this->message === null) {
+            $this->message = Yii::t('app', '{property}: {message}.');
+        }
+
+        if ($this->notString === null) {
+            $this->notString = Yii::t('app', 'The value must be a string.');
+        }
+
+        if ($this->notJsonString === null) {
+            $this->notJsonString = Yii::t('app', 'The value must be a valid JSON string.');
+        }
+
+        if (empty($this->schema)) {
+            throw new InvalidConfigException($this->schemaEmpty);
+        }
+
+        if (!is_string($this->schema)) {
+            throw new InvalidConfigException($this->schemaNotString);
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function validateAttribute($model, $attribute)
+    {
+        if (!is_string($model->$attribute)) {
+            $this->addError($model, $attribute, $this->notString);
+            return;
+        }
+
+        $value = json_decode($model->$attribute);
+        if (json_last_error()) {
+            $this->addError($model, $attribute, $this->notJsonString);
+        }
+
+        $retriever = new UriRetriever();
+        $schema = $retriever->retrieve($this->schema);
+
+        $validator = new JSValidator();
+        $validator->check($value, $schema);
+
+        if (!$validator->isValid()) {
+            foreach ($validator->getErrors() as $error) {
+                $this->addError(
+                    $model,
+                    $attribute,
+                    $this->message,
+                    [
+                        'property' => $error['property'],
+                        'message' => ucfirst($error['message']),
+                    ]
+                );
+            }
+        }
+    }
+
+    /**
+     * Validates a value.
+     *
+     * @param string $value A JSON encoded string to validate.
+     * @return array|null An array of error data, or null if the data is valid.
+     */
+    protected function validateValue($value)
+    {
+        if (!is_string($value)) {
+            return [$this->notString, []];
+        }
+
+        $value = json_decode($value);
+        if (json_last_error()) {
+            return [$this->notJsonString, []];
+        }
+
+        $retriever = new UriRetriever();
+        $schema = $retriever->retrieve($this->schema);
+
+        $validator = new JSValidator();
+        $validator->check($value, $schema);
+
+        if (!$validator->isValid()) {
+            $errors = $validator->getErrors();
+            $error = reset($errors);
+            return [$this->message, ['property' => $error['property'], 'message' => ucfirst($error['message'])]];
+        }
+
+        return null;
+    }
+}
